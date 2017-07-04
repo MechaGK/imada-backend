d886f341dff8b29305b692f0652df9adeb2218de
Mads Grau Kristensen <madsgrau@gmail.com>
2017-07-04 23:17:46 +0200

    Added 'spend' and 'addFunds' endpoints

--- common/models/imada-user.js
+++ common/models/imada-user.js
@@ -1,5 +1,70 @@
 'use strict';
 
-module.exports = function(Imadauser) {
+module.exports = function (Imadauser) {
+    Imadauser.spend = function (id, amount, time, options, cb) {
+        Imadauser.findById(id, function (err, user) {
+            if (err) return cb(err);
 
+            var canSpend = user.balance >= amount;
+            if (canSpend) {
+                Imadauser.app.models.Transaction.create({
+                    imadaUserId: id,
+                    amount: -amount,
+                    timeOnDevice: time
+                }, function (err, transaction) {
+                    if (err) return cb(err);
+
+                    user.balance -= amount;
+                    user.save();
+
+                    cb(null, true, transaction.id);
+                });
+            } else {
+                cb(null, false, undefined);
+            }
+        })
+    };
+    Imadauser.remoteMethod('spend', {
+        accepts: [
+            { arg: 'id', type: 'number', required: true },
+            { arg: 'amount', type: 'number', required: true },
+            { arg: 'time', type: 'date' },
+            { arg: 'options', type: 'object', http: 'optionsFromRequest' }
+        ],
+        returns: [
+            { arg: 'success', type: 'boolean' },
+            { arg: 'transactionId', type: 'number' }
+        ],
+        http: { path: '/spend', verb: 'post' }
+    });
+    Imadauser.addFunds = function(id, amount, options, cb) {
+        Imadauser.findById(id, function(err, user) {
+            if (err) return cb(err);
+
+            Imadauser.app.models.Transaction.create({
+                    imadaUserId: id,
+                    amount: amount,
+                    authorizedBy: options.accessToken.userId
+                }, function (err, transaction) {
+                    if (err) return cb(err);
+
+                    user.balance += amount;
+                    user.save();
+
+                    cb(null, true, transaction.id);
+                });
+        })
+    };
+    Imadauser.remoteMethod('addFunds', {
+        accepts: [
+            { arg: 'id', type: 'number', required: true },
+            { arg: 'amount', type: 'number', required: true },
+            { arg: 'options', type: 'object', http: 'optionsFromRequest' }
+        ],
+        returns: [
+            { arg: 'success', type: 'boolean' },
+            { arg: 'transactionId', type: 'number' }
+        ],
+        http: { path: '/addFunds', verb: 'post' }
+    });
 };
--- server/boot/my-boot-script.js
+++ server/boot/my-boot-script.js
@@ -1,24 +1,39 @@
 module.exports = function (app) {
     // Creating admin account
     var User = app.models.ImadaUser;
-    User.create({ username: "admin", email: 'admin@mechagk.dk', password: 'StrongPassword' }, function (err, user) {
-        if (err) return debug('%j', err);
+    var Role = app.models.Role;
+    var RoleMapping = app.models.RoleMapping;
+    User.create({ username: 'admin', email: 'admin@mechagk.dk', password: 'pleaseChangeMe' }, function (err, user) {
+        if (err) throw err;
 
-        role.create({
+        Role.create({
             name: 'admin'
         }, function (err, role) {
-            if (err) return debug(err);
-            debug(role);
+            if (err) throw err;
+            console.log(role);
 
             role.principals.create({
                 principalType: RoleMapping.USER,
                 principalId: user.id
             }, function (err, principal) {
-                if (err) return debug(err);
-                debug(principal);
+                if (err) throw err;
+                console.log(principal);
             });
         })
     });
 
-    console.log("Remember to change admin password from 'StrongPassword'!")
+    console.log("Remember to change admin password from 'pleaseChangeMe'!");
+
+    // For testing only
+    User.create({ username: 'tester', email: 'tester@mechagk.dk', password: 'WeakPassword', balance: 600}, function(err, user) {
+        if (err) throw err;
+
+        console.log(user);
+    });
+
+    User.create({ username: 'tester2', email: 'tester2@mechagk.dk', password: 'WeakPassword'}, function(err, user) {
+        if (err) throw err;
+
+        console.log(user);
+    });
 }
