7ba6dd21c707ec197a0e3bc98618339a6b581a31
Mads Grau Kristensen <madsgrau@gmail.com>
2017-11-03 01:02:54 +0100

    Stop trying to create admin if role is not created yet

--- server/boot/my-boot-script.js
+++ server/boot/my-boot-script.js
@@ -3,35 +3,55 @@ module.exports = function (app) {
     var Role = app.models.Role;
     var RoleMapping = app.models.RoleMapping;
 
-    User.exists(1, function (err, exists) {
+    Role.exists(1, function (err, exists) {
         if (!exists) {
-            randomString = function(length, chars) {
-                var result = '';
-                for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
-                return result;
-            }
-            var adminPassword = randomString(24, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
-        
-            // Creating admin account
-            
-            User.create({ username: 'admin', email: 'admin@mechagk.dk', password: adminPassword }, function (err, user) {
-                if (err) throw err;
-        
-                Role.create({
-                    name: 'admin'
-                }, function (err, role) {
-                    if (err) throw err;
-        
-                    role.principals.create({
-                        principalType: RoleMapping.USER,
-                        principalId: user.id
-                    }, function (err, principal) {
-                        if (err) throw err;
-                    });
-                })
+            Role.create({
+                name: 'admin'
+            }, function (err, role) {
+                if (err) {
+                    console.log('Could not create "admin" role. Loopback models are probably not migrated yet');
+                    return;
+                }
+
+                // Admin cannot be created before 'admin' role, so we do not check if it exists
+                createAdmin(User, RoleMapping, role);
             });
-        
-            console.log("Admin user created.\n\temail: admin@mechagk.dk\n\tpassword: " + adminPassword);
         }
+        else {
+            User.exists(1, function (err, exists) {
+                if (!exists) {
+                    role = Role.findById(1, function (err, role) {
+                        if (err) throw err;
+
+                        createAdmin(User, RoleMapping, role)
+                    });
+                }
+            });
+        }
+    });
+}
+
+randomString = function (length, chars) {
+    var result = '';
+    for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+    return result;
+}
+
+createAdmin = function (User, RoleMapping, role) {
+    var adminPassword = randomString(24, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+
+    User.create({ username: 'admin', email: 'admin@mechagk.dk', password: adminPassword }, function (err, user) {
+        if (err) {
+            console.log('Could not create admin. Imada models are probably not migrated yet');
+            return;
+        }
+
+        role.principals.create({
+            principalType: RoleMapping.USER,
+            principalId: user.id
+        }, function (err, principal) {
+            if (err) throw err;
+        });
+        console.log("Admin user created.\n\temail: admin@mechagk.dk\n\tpassword: " + adminPassword);
     });
 }
